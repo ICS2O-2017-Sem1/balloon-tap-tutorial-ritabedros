d6c9a6bf31be73969ea553d5a0da3eb317bc1b7b
ritabedros <s435131@ocsb.local>
2017-09-19 11:17:04 -0400

    first day

--- main.lua
+++ main.lua
@@ -4,4 +4,10 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- local backgroung = display.newImageRect("background.png", 360, 570)
+background.x = display.conentCenterX
+background.y = display.conentCentreY 
+
+local platform = display.newImageRect( "platform.png", 360, 50)
+platform.x = display.contentCenterX
+platform.y = display.conentCentre-25
